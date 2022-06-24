4b607543569adefc18aec57f7fd31063e1cc8b5c
pablopalou <64150071+pablopalou@users.noreply.github.com>
2022-06-24 15:26:18 -0300

    Revert "Feature/hf234sd/next auth"

--- front/components/routes/routes.tsx
+++
@@ -1,6 +0,0 @@
-export const taskHistory = "/submission?role=doctor";
-export const newSubmission = "/submission/create";
-export const allSubmissions = "/submission/all";
-export const home = "/";
-export const login = "/login";
-export const register = "/register";
