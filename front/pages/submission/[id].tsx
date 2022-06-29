4d47951eddbecf9136548be327ecdcb8a09c7a2e
Pablo Palou <ppalou2000@icloud.com>
2022-06-29 16:50:02 -0300

    DetailSubmission

--- front/pages/submission/[id].tsx
+++ front/pages/submission/[id].tsx
@@ -44,7 +44,7 @@ const SubmissionDetailPage:NextPage = () => {
 
     return (
         <Layout>
-            <div className='w-full flex justify-center'>
+            <div className='w-full flex justify-center pb-10'>
                 <div className='w-11/12 flex flex-col mt-10'>
                     <Link href={routes.home} passHref><a className="text-blue-500 hover:text-blue-800 mb-8"> <img src="../icons/backarrow.svg" alt="" /></a></Link>
                     {/* div for id, status and doctor*/}
@@ -58,8 +58,41 @@ const SubmissionDetailPage:NextPage = () => {
                     </div>
 
                     {/* patient info */}
-                    <div>
+                    <div className='flex flex-col mb-4'>
+                        <h5> Patient Information:</h5>
+                        <div className='flex w-full'>
+                            <p className='w-1/2'> Email </p>
+                            <p className='w-1/2'> Name </p>
+                        </div>
+                        <div className='flex mb-3'>
+                            <div className='w-1/2'> {submission?.patient.email} </div>
+                            <div className='w-1/2'> {submission?.patient.name} </div>
+                        </div>
+                        <div className='flex w-full'>
+                            <p className='w-1/2'> Birth </p>
+                            <p className='w-1/2'> Gender </p>
+                        </div>
+                        <div className='flex mb-3'>
+                            <div className='w-1/2'> {submission?.patient.patientInformation.birth} </div>
+                            <div className='w-1/2'> {submission?.patient.patientInformation.gender} </div>
+                        </div>
+                        <div className='flex w-full'>
+                            <p className='w-1/2'> Height </p>
+                            <p className='w-1/2'> Weight </p>
+                        </div>
+                        <div className='flex mb-3'>
+                            <div className='w-1/2'> {submission?.patient.patientInformation.height} </div>
+                            <div className='w-1/2'> {submission?.patient.patientInformation.weight} </div>
+                        </div>
 
+                        <div className=' mb-3'>
+                            <p> Diseases </p>
+                            <div>{submission?.patient.patientInformation.diseases}</div>
+                        </div>
+                        <div>
+                            <p> Previous treatments </p>
+                            <div>{submission?.patient.patientInformation.previous_treatments}</div>
+                        </div>
                     </div>
 
                     {/* symptoms */}
@@ -72,8 +105,9 @@ const SubmissionDetailPage:NextPage = () => {
                         <h5 className='mb-0'> Prescriptions </h5>
                         { submission?.prescriptions ? 
                             (<div className='bg-gray-50'> <p>submission?.prescriptions </p></div>) :
-                            (<div className='bg-gray-50'>
-                                No prescriptions have been added yet
+                            (<div className='bg-gray-200 py-3 flex pl-4 '>
+                                <img src="../icons/notavailable.svg" alt="" />
+                                <p className='text-lg pl-3'>No prescriptions have been added yet</p>
                             </div>)
                         }
                     </div>
